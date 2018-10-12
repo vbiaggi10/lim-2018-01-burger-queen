13bfe8623afb7060abaf112a1f0a999ddea7c029
Valeria <vbiaggi10@gmail.com>
2018-10-12 10:50:53 -0500

    delete order

--- src/components/take_order/TakeOrder.js
+++ src/components/take_order/TakeOrder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Tabs, Tab, Container} from 'react-materialize';
+import {Tabs, Tab, Container, Icon} from 'react-materialize';
 import Breakfast from './Breakfast';
 import LunchDinner from './LunchDinner';
 
@@ -9,9 +9,11 @@ class TakeOrder extends Component {
     this.state = {
       orders: []
     }
+    this.myRef = React.createRef();
   }
   render() {
-    console.log(this.totalPrice())
+    const price = this.totalPrice();    
+    const totalPrice = price[price.length - 1];
     return (
       <div className="row">
         <h1>{this.props.name}'s order</h1>
@@ -24,6 +26,7 @@ class TakeOrder extends Component {
         <Container className="col s4">
           <h3>Order</h3>
           {this.showOrder()}
+          <p className="total-price">Total: {totalPrice ? totalPrice : 0}</p>
         </Container>
       </div>
     );
@@ -32,7 +35,11 @@ class TakeOrder extends Component {
   showOrder() {
     if(this.state.orders !== []){
       return this.state.orders.map( (order, i) => {
-        return (<p key={`show${i}`}>{order.orderName} S/ {order.orderPrice}</p>)
+        return (
+          <div key={`show${i}`}>
+            <span>{order.orderName} S/ {order.orderPrice}</span> 
+            <a className='btn-delete' ref={this.myRef} name={order.orderName} onClick={this.deleteOrder.bind(this)}><Icon small>delete</Icon></a>
+          </div>)
       })
     }
   }
@@ -56,6 +63,19 @@ class TakeOrder extends Component {
       })
     }
   }
+
+  deleteOrder(e) {
+    const node = this.myRef.current;
+    const orders = this.state.orders;
+    orders.forEach(order => {
+      if (order.orderName === node.name) {
+        const indexUpdate = orders.indexOf(order)
+        delete orders[indexUpdate];
+      }
+    })
+    console.log(this.state.orders)
+  }
+  
 }
 
 export default TakeOrder;
