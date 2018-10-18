4e355440b1727ec04c8cd8c59d83c4d52f8d7571
vbiaggi10 <vbiaggi10@gmail.com>
2018-10-18 16:10:08 -0500

    Updates

--- precache-manifest.5e57cebe015de3e378301c32daa93b94.js
+++ precache-manifest.5e57cebe015de3e378301c32daa93b94.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
   },
   {
-    "revision": "5fe362659614b29aceec",
-    "url": "/lim-2018-01-burger-queen/static/js/main.5fe36265.chunk.js"
+    "revision": "9776bda138188ab71725",
+    "url": "/lim-2018-01-burger-queen/static/js/main.9776bda1.chunk.js"
   },
   {
     "revision": "b6fca53660acfd725c58",
     "url": "/lim-2018-01-burger-queen/static/js/1.b6fca536.chunk.js"
   },
   {
-    "revision": "5fe362659614b29aceec",
+    "revision": "9776bda138188ab71725",
     "url": "/lim-2018-01-burger-queen/static/css/main.3caa1744.chunk.css"
   },
   {
-    "revision": "a97ffd97d9eda18e2965260240ff6b51",
+    "revision": "457959ebca720c13bcf4a07f004356b5",
     "url": "/lim-2018-01-burger-queen/index.html"
   }
 ];
