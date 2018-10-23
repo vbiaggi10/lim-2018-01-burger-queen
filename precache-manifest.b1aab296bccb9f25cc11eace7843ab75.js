8eff623acfb4a110cc5a9d32be123069d76be5ba
Valeria <vbiaggi10@gmail.com>
2018-10-22 21:38:37 -0500

    Updates

---
+++ precache-manifest.b1aab296bccb9f25cc11eace7843ab75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0dab808f1c361c40aacb",
+    "url": "/lim-2018-01-burger-queen/static/js/runtime~main.0dab808f.js"
+  },
+  {
+    "revision": "a3f1ef10746cfd4f1a6f",
+    "url": "/lim-2018-01-burger-queen/static/js/main.a3f1ef10.chunk.js"
+  },
+  {
+    "revision": "04525ebf48ef9b46368a",
+    "url": "/lim-2018-01-burger-queen/static/js/1.04525ebf.chunk.js"
+  },
+  {
+    "revision": "a3f1ef10746cfd4f1a6f",
+    "url": "/lim-2018-01-burger-queen/static/css/main.db24fbde.chunk.css"
+  },
+  {
+    "revision": "711b8554175cf71d0517ee01d1d3a340",
+    "url": "/lim-2018-01-burger-queen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/lim-2018-01-burger-queen/precache-manifest.c2948c8dfeed0b20c22b5b337b41275f.js"
+  "/lim-2018-01-burger-queen/precache-manifest.b1aab296bccb9f25cc11eace7843ab75.js"
 );
 
 workbox.clientsClaim();
