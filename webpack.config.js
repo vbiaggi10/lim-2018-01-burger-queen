d4df02d4c89f7f0b85dbefa4aba7e800a530b3e4
Valeria <vbiaggi10@gmail.com>
2018-10-24 22:43:32 -0500

    package

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
   entry: path.join(__dirname, "./src/index.js"),
   output: {
     path: path.join(__dirname, "./public"),
-    filename: "bundle.js",
-    publicPath: "/webpack-demo/"
+    filename: "bundle.js"
   },
   module: {
     rules: [{
